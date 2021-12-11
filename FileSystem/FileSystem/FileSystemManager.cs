eb774c5046c9db310c6c1e0a959efb1882e6f3c5
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2021-12-11 18:43:23 +0300

    Added FileSystemVisitor functionality

---
+++ FileSystem/FileSystem/FileSystemManager.cs
@@ -0,0 +1,16 @@
+﻿using FileSystem;
+
+internal class FileSystemManager
+{
+    static void Main(string[] args)
+    {
+        var dir = @"C:\Users\Kiryl_Kartashou\Desktop\Files";
+
+        var fileSystemVisitor = new FileSystemVisitor(dir);
+
+        foreach (var fileInfo in fileSystemVisitor.GetFilesInfo())
+        {
+            Console.WriteLine(fileInfo);
+        }
+    }
+}
---
+++ FileSystem/FileSystem/FileSystemVisitor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    internal class FileSystemVisitor : IFileSystemVisitor
+    {
+        private DirectoryInfo _dir;
+
+        public FileSystemVisitor(string dir)
+        {
+            _dir = new DirectoryInfo(dir);
+        }
+
+        public IEnumerable<FileSystemInfo> GetFilesInfo()
+        {
+            return GetFilesInfoFromDir(_dir);
+        }
+
+        public IEnumerable<FileSystemInfo> GetFilesInfoFromDir(DirectoryInfo dir)
+        {
+            DirectoryInfo subDir;
+
+            foreach (var fileInfo in dir.EnumerateFileSystemInfos())
+            {
+                subDir = fileInfo as DirectoryInfo;
+
+                yield return fileInfo;
+
+                if (subDir != null)
+                {
+                    foreach (var subFile in GetFilesInfoFromDir(subDir))
+                    {
+                        yield return subFile;
+                    }
+                }
+            }
+        }
+    }
+}
