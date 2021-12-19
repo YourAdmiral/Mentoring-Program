5b2b6da71c9d070b8bcb690b92baa7b0410e69ad
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2021-12-19 21:12:51 +0300

    Added tests

---
+++ FileSystemWithTests/FileSystemTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileSystemTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("FileSystemTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e941ed3-0f89-4023-8a19-1d54c6bee5b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FileSystemWithTests/FileSystemTests/UnitTests.cs
@@ -0,0 +1,194 @@
+﻿using FileSystemWithTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileSystemTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void GetFilesInfoReturnsAllDirectoriesAndFiles()
+        {
+            var dir = @"C:\Users\Kiryl_Kartashou\Desktop\Files";
+
+            var fileSystemVisitor = new FileSystemVisitor(dir);
+
+            List<FileSystemInfo> elements = new List<FileSystemInfo>();
+
+            List<FileSystemInfo> allElements = new List<FileSystemInfo>()
+            {
+                new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog"),
+                new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\cat"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file.txt"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file2.txt"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file3.txt"),
+                new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog2"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog2\myfile.txt")
+            };
+
+            foreach (var fileInfo in fileSystemVisitor.GetFilesInfo())
+            {
+                elements.Add(fileInfo);
+            }
+
+            CollectionAssert.AreEqual(
+                allElements.Select(f => f.FullName).ToList(), 
+                elements.Select(f => f.FullName).ToList());
+        }
+
+        [TestMethod]
+        public void FilterReturnsFile2txt()
+        {
+            var dir = @"C:\Users\Kiryl_Kartashou\Desktop\Files";
+
+            var fileSystemVisitor = new FileSystemVisitor(dir, file => file.Equals("file2.txt"));
+
+            List<FileSystemInfo> elements = new List<FileSystemInfo>();
+
+            FileInfo filteredFileInfo = new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file2.txt");
+
+            foreach (var fileInfo in fileSystemVisitor.GetFilesInfo())
+            {
+                elements.Add(fileInfo);
+            }
+
+            Assert.AreEqual(
+                filteredFileInfo.FullName,
+                elements.FirstOrDefault().FullName);
+        }
+
+        [TestMethod]
+        public void CheckExcludedDirectories()
+        {
+            var dir = @"C:\Users\Kiryl_Kartashou\Desktop\Files";
+
+            var fileSystemVisitor = new FileSystemVisitor(dir);
+
+            List<FileSystemInfo> elementsWithoutExcludedDirectories = new List<FileSystemInfo>()
+            {
+                new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\cat"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file.txt"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file2.txt"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file3.txt"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog2\myfile.txt")
+            };
+
+            List<FileSystemInfo> elements = new List<FileSystemInfo>();
+
+            List<DirectoryInfo> excludedDirectories = new List<DirectoryInfo>();
+
+            excludedDirectories.Add(new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog"));
+
+            excludedDirectories.Add(new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog2"));
+
+            foreach (var fileInfo in fileSystemVisitor.GetFilesInfo())
+            {
+                elements.Add(fileInfo);
+            }
+
+            fileSystemVisitor.ExcludeDirectoriesFromList(elements, excludedDirectories);
+
+            CollectionAssert.AreEqual(
+                elementsWithoutExcludedDirectories.Select(f => f.FullName).ToList(),
+                elements.Select(f => f.FullName).ToList());
+        }
+
+        [TestMethod]
+        public void CheckExcludedFilesByName()
+        {
+            var dir = @"C:\Users\Kiryl_Kartashou\Desktop\Files";
+
+            var fileSystemVisitor = new FileSystemVisitor(dir);
+
+            List<FileSystemInfo> elementsWithoutExcludedFiles = new List<FileSystemInfo>()
+            {
+                new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog"),
+                new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\cat"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file.txt"),
+                new FileInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog\file3.txt"),
+                new DirectoryInfo(@"C:\Users\Kiryl_Kartashou\Desktop\Files\Catalog2")
+            };
+
+            List<FileSystemInfo> elements = new List<FileSystemInfo>();
+
+            List<string> excludedFiles = new List<string>();
+
+            excludedFiles.Add("file2.txt");
+
+            excludedFiles.Add("myfile.txt");
+
+            fileSystemVisitor.ExcludeFilesFromListByName(elements, excludedFiles);
+
+            foreach (var fileInfo in fileSystemVisitor.GetFilesInfo())
+            {
+                elements.Add(fileInfo);
+            }
+
+            fileSystemVisitor.ExcludeFilesFromListByName(elements, excludedFiles);
+
+            CollectionAssert.AreEqual(
+                elementsWithoutExcludedFiles.Select(f => f.FullName).ToList(),
+                elements.Select(f => f.FullName).ToList());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetFilesInfo_ThrowsArgumentNullException()
+        {
+            string dir = null;
+
+            var fileSystemVisitor = new FileSystemVisitor(dir);
+
+            List<FileSystemInfo> elements = new List<FileSystemInfo>();
+
+            foreach (var fileInfo in fileSystemVisitor.GetFilesInfo())
+            {
+                elements.Add(fileInfo);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExcludeDirectoriesFromList_ThrowsArgumentNullException()
+        {
+            var dir = @"C:\Users\Kiryl_Kartashou\Desktop\Files";
+
+            var fileSystemVisitor = new FileSystemVisitor(dir);
+
+            List<FileSystemInfo> elements = new List<FileSystemInfo>();
+
+            foreach (var fileInfo in fileSystemVisitor.GetFilesInfo())
+            {
+                elements.Add(fileInfo);
+            }
+
+            List<DirectoryInfo> excludedDirectories = null;
+
+            fileSystemVisitor.ExcludeDirectoriesFromList(elements, excludedDirectories);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExcludeFilesFromList_ThrowsArgumentNullException()
+        {
+            var dir = @"C:\Users\Kiryl_Kartashou\Desktop\Files";
+
+            var fileSystemVisitor = new FileSystemVisitor(dir);
+
+            List<FileSystemInfo> elements = new List<FileSystemInfo>();
+
+            foreach (var fileInfo in fileSystemVisitor.GetFilesInfo())
+            {
+                elements.Add(fileInfo);
+            }
+
+            List<FileInfo> excludedFiles = null;
+
+            fileSystemVisitor.ExcludeFilesFromList(elements, excludedFiles);
+        }
+    }
+}
