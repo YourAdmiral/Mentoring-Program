3fcff47e84b1cb74bc93e3de6862cb4d04ab3c60
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2021-12-25 19:21:19 +0300

    Implemented task1

--- Exceptions handling/Task1/Program.cs
+++ Exceptions handling/Task1/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            try
+            {
+                string str;
+
+                Console.WriteLine("Input some value: ");
+
+                str = Console.ReadLine().Trim();
+
+                if (str == null || str.Trim() == "")
+                {
+                    throw new Exception("Value is not valid");
+                }
+
+                Console.WriteLine("Input: '" + str + "'");
+                Console.WriteLine("First character: " + str.Substring(0, 1));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
