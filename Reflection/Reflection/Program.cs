4c6268381ac73450febe12de5d3e6d7239776391
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-01-13 22:37:15 +0300

    Added load/save functionality for fileConfigurationProvider

--- Reflection/Reflection/Program.cs
+++ Reflection/Reflection/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Reflection.Class_examples;
+using Reflection.Providers;
+using System;
+using System.IO;
 
 namespace Reflection
 {
@@ -6,7 +9,23 @@ namespace Reflection
     {
         static void Main(string[] args)
         {
+            FileConfigurationProvider fileProvider = new FileConfigurationProvider(@"..\..\..\TextFile.txt");
 
+            //fileProvider.SaveSetting("str, string");
+
+            //fileProvider.SaveSetting("value, int");
+
+            //string str = fileProvider.LoadSetting();
+
+            //Console.WriteLine(str);
+
+            CustomFile file = new CustomFile(
+                1,
+                15,
+                "Text",
+                new TimeSpan(4, 20, 40));
+
+            file.SaveSettings();
         }
     }
 }
--- Reflection/Reflection/Providers/FileConfigurationProvider .cs
+++ Reflection/Reflection/Providers/FileConfigurationProvider .cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Reflection.Attributes;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,27 @@ namespace Reflection.Providers
 {
     internal class FileConfigurationProvider
     {
+        public string Path { get; set; }   
+
+        public FileConfigurationProvider(string path)
+        {
+            Path = path;
+        }
+
+        public void SaveSetting(ConfigurationItemAttribute setting)
+        {
+            using (StreamWriter sw = new StreamWriter(Path, true))
+            {
+                sw.WriteLine(setting.Name);
+            }
+        }
+
+        public string LoadSetting(ConfigurationItemAttribute setting)
+        {
+            using (StreamReader sr = new StreamReader(Path))
+            {
+                return sr.ReadToEnd();
+            }
+        }
     }
 }
