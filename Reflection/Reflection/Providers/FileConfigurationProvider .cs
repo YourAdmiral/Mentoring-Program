bacf3c9a82ebed8a7b052be4a2a11f03031fd5a4
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-01-16 15:51:08 +0300

    Changed file saving

--- Reflection/Reflection/Providers/FileConfigurationProvider .cs
+++ Reflection/Reflection/Providers/FileConfigurationProvider .cs
@@ -23,63 +23,100 @@ namespace Reflection.Providers
             PropertyInfo setting, 
             object value)
         {
-            using (StreamWriter sw = new StreamWriter(Path, true))
+            List<List<string>> settingsList = LoadFileSettings();
+
+            List<string> savedSetting = settingsList.FirstOrDefault(item => item[0] == setting.Name);
+
+            if (savedSetting == null)
             {
-                sw.WriteLine($"{setting.Name},{value}");
+                using (StreamWriter sw = new StreamWriter(Path, true))
+                {
+                    sw.WriteLine($"{setting.Name},{value}");
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(Path, false))
+                {
+                    sw.Write("");
+
+                    foreach (var settings in settingsList)
+                    {
+                        if (settings[0] == setting.Name)
+                        {
+                            settings[1] = value.ToString();
+                        }
+
+                        sw.WriteLine($"{settings[0]},{settings[1]}");
+                    }
+                }
             }
         }
 
         public object LoadSetting(PropertyInfo setting)
         {
-            List<string> settingValues;
+            Type propType = setting.PropertyType;
 
+            List<List<string>> settingsList = LoadFileSettings();
+
+            foreach (var settingValues in settingsList)
+            {
+                if (settingValues[0] == setting.Name)
+                {
+                    return GetSettingValue(propType, settingValues[1]);
+                }
+            }
+
+            return null;
+        }
+
+        private object GetSettingValue(Type propType, string propValue)
+        {
             int intValue;
 
             float floatValue;
 
-            List<string> stringList = new List<string>();
-
-            List<List<string>> settingsList = new List<List<string>>();
-
             TimeSpan timeSpanValue;
 
-            Type propType = setting.PropertyType;
+            if (propType == typeof(int))
+            {
+                intValue = Convert.ToInt32(propValue);
+
+                return intValue;
+            }
+            else if (propType == typeof(float))
+            {
+                floatValue = Convert.ToSingle(propValue);
+
+                return floatValue;
+            }
+            else if (propType == typeof(TimeSpan))
+            {
+                timeSpanValue = TimeSpan.Parse(propValue);
+
+                return timeSpanValue;
+            }
+            else if (propType == typeof(string))
+            {
+                return propValue;
+            }
+
+            return null;
+        }
+
+        private List<List<string>> LoadFileSettings()
+        {
+            List<List<string>> settingsList = new List<List<string>>();
 
             using (StreamReader sr = new StreamReader(Path))
             {
                 while (!sr.EndOfStream)
                 {
-                    settingValues = sr.ReadLine().Split(',').ToList();
-
-                    if (settingValues[0] == setting.Name)
-                    {
-                        if (propType == typeof(int))
-                        {
-                            intValue = Convert.ToInt32(settingValues[1]);
-
-                            return intValue;
-                        }
-                        else if (propType == typeof(float))
-                        {
-                            floatValue = Convert.ToSingle(settingValues[1]);
-
-                            return floatValue;
-                        }
-                        else if (propType == typeof(TimeSpan))
-                        {
-                            timeSpanValue = TimeSpan.Parse(settingValues[1]);
-
-                            return timeSpanValue;
-                        }
-                        else if (propType == typeof(string))
-                        {
-                            return settingValues[1];
-                        }
-                    }
+                    settingsList.Add(sr.ReadLine().Split(',').ToList());
                 }
             }
 
-            return null;
+            return settingsList;
         }
     }
 }
