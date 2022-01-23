8704bc4831d785f6d1e991ff2eea48cdbe7aa55b
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-01-23 21:53:33 +0300

    Implemented HybridFlowProcessor

--- DataStructures/Tasks/HybridFlowProcessor.cs
+++ DataStructures/Tasks/HybridFlowProcessor.cs
@@ -5,24 +5,47 @@ namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private IDoublyLinkedList<T> _storage;
+
+        public HybridFlowProcessor()
+        {
+            _storage = new DoublyLinkedList<T>();
+        }
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (_storage.Length > 0)
+            {
+                return _storage.RemoveAt(0);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            _storage.Add(item);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (_storage.Length > 0)
+            {
+                int lastIndex = _storage.Length - 1;
+
+                return _storage.ElementAt(lastIndex);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _storage.Add(item);
         }
     }
 }
