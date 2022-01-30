68aae81340e206247cef7531f975f8453450d875
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-01-30 21:27:20 +0300

    Implement Sum

--- StringSum/StringSum/StringSummer.cs
+++ StringSum/StringSum/StringSummer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringSum
 {
@@ -6,7 +7,50 @@ namespace StringSum
     {
         public static string Sum(string num1, string num2)
         {
-            return null;
+            var firstNumber = NumberParser.Parse(num1);
+
+            var secondNumber = NumberParser.Parse(num2);
+
+            int finalNumber = 0;
+
+            var numbers = new Dictionary<int, bool>();
+
+            numbers.Add(firstNumber, IsNaturalNumber(firstNumber));
+
+            numbers.Add(secondNumber, IsNaturalNumber(secondNumber));
+
+            foreach (var item in numbers)
+            {
+                if (item.Value)
+                {
+                    finalNumber += item.Key;
+                }
+            }
+
+            return finalNumber.ToString();
+        }
+
+        private static bool IsNaturalNumber(int num)
+        {
+            bool isNatural = true;
+
+            if (num <= 0)
+            {
+                isNatural = false;
+            }
+            else
+            {
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        isNatural = false;
+                        break;
+                    }
+                }
+            }
+
+            return isNatural;
         }
     }
 }
