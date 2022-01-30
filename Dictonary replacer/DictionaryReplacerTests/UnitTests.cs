dbf87f4d8f1dbee98e976cd01492f63c67db7d52
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-01-30 22:11:22 +0300

    Add empty projects

---
+++ Dictonary replacer/DictionaryReplacerTests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DictionaryReplacerTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
---
+++ Dictonary replacer/Dictonary replacer/DictionaryReplacer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Dictonary_replacer
+{
+    public class DictionaryReplacer
+    {
+    }
+}
