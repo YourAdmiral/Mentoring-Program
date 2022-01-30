c7d46130a8f405d69b16e08e43b660c5d5c473e2
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-01-31 01:51:48 +0300

    Implement IsLeapYear

--- Leap year/Leap year/LeapYear.cs
+++ Leap year/Leap year/LeapYear.cs
@@ -6,7 +6,13 @@ namespace Leap_year
     {
         public static bool IsLeapYear(int year)
         {
-            return false;
+            if (year < 1 
+                || year > 9999)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return (year % 4) == 0 && (year % 100 != 0 || year % 400 == 0);
         }
     }
 }
