74b79d64f69f59d1dd6c36c7604a0a9deaf7ac36
MANDOM <yourmarshall@yandex.by>
2022-02-06 18:28:03 +0300

    Post deployment scripts merged into one script

--- SqlDb/SqlDb/PostDeployScripts/PostDeploymentScript.sql
+++ SqlDb/SqlDb/PostDeployScripts/PostDeploymentScript.sql
@@ -14,3 +14,21 @@ VALUES (1, 'Alex','Smith'),
 (2, 'Robert','Cooper'),
 (3, 'Katelyn','Black'),
 (4, 'Jessica','Stein');
+
+INSERT INTO Address(Id, Street, City, State, ZipCode)
+VALUES (1, 'West st.','Austin', 'Texas', '54452'),
+(2, 'Some st.','Houston', 'Texas', '23411'),
+(3, 'Walker st.','Dallas', 'San Antonio', '54452'),
+(4, 'Green st.','Tampa', 'Florida', '741123');
+
+INSERT INTO Company(Id, Name, AddressId)
+VALUES (1, 'Google',4),
+(2, 'Amazon', 3),
+(3, 'Apple', 2),
+(4, 'Microsoft', 1);
+
+INSERT INTO Employee(Id, AddressId, PersonId, CompanyName, Position, EmployeeName)
+VALUES (1, 4, 1, 'Google', 'Software Engineer', 'Alex'),
+(2, 3, 2,'Amazon', 'Economist', 'Robert'),
+(3, 2, 3, 'Apple', 'Director', 'Katelyn'),
+(4, 1, 4, 'Microsoft', 'Support engineer', 'Jessica');
