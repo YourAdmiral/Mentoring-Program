029308cfde6621352768dc36d2d625923ba2f1dc
MANDOM <yourmarshall@yandex.by>
2022-02-06 17:21:23 +0300

    Added publish and changed constraints

--- SqlDb/SqlDb/Company.sql
+++ SqlDb/SqlDb/Company.sql
@@ -2,5 +2,6 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(20) NOT NULL, 
-    [AddressId] INT NOT NULL FOREIGN KEY REFERENCES Address(Id)
+    [AddressId] INT NOT NULL,
+    CONSTRAINT [FK_AddressId] FOREIGN KEY ([AddressId]) REFERENCES [Address]([Id])
 )
