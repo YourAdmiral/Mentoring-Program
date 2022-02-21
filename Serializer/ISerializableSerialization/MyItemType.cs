ac512cd447cbe3c160b141d74fdd54641c835a38
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-02-21 22:18:48 +0300

    Created ISerializable serializer

---
+++ Serializer/ISerializableSerialization/MyItemType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace ISerializableSerialization
+{
+    [Serializable]
+    public class MyItemType : ISerializable
+    {
+        public MyItemType()
+        {
+
+        }
+        
+        private string firstProperty_value;
+
+        private string secondProperty_value;
+
+        public string FirstProperty
+        {
+            get { return firstProperty_value; }
+            set { firstProperty_value = value; }
+        }
+
+        public string SecondProperty
+        {
+            get { return secondProperty_value; }
+            set { secondProperty_value = value; }
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("props1", firstProperty_value, typeof(string));
+            info.AddValue("props2", secondProperty_value, typeof(string));
+        }
+
+        public MyItemType(SerializationInfo info, StreamingContext context)
+        {
+            firstProperty_value = (string)info.GetValue("props1", typeof(string));
+            secondProperty_value = (string)info.GetValue("props2", typeof(string));
+        }
+    }
+}
---
+++ Serializer/ISerializableSerialization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ISerializableSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "dataStuff.myData";
+            
+            IFormatter formatter = new BinaryFormatter();
+            
+            MyItemType t1 = new MyItemType();
+            t1.FirstProperty = "Hello";
+            t1.SecondProperty = "World";
+
+            FileStream s1 = new FileStream(fileName, FileMode.Create);
+            formatter.Serialize(s1, t1);
+            s1.Close();
+
+            FileStream s = new FileStream(fileName, FileMode.Open);
+            MyItemType t = (MyItemType)formatter.Deserialize(s);
+            Console.WriteLine(t.FirstProperty);
+            Console.WriteLine(t.SecondProperty);
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
