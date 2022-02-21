3bcbc3560c817564f598a4c2d0ef888e8816bb7a
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-02-21 22:47:22 +0300

    Created deep clone serializer

---
+++ Serializer/DeepCloningSerialization/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepCloningSerialization
+{
+    [Serializable]
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
---
+++ Serializer/DeepCloningSerialization/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace DeepCloningSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string employeeName = "Nate";
+
+            string departmentName = "Development department";
+
+            string jsonFile = "Department.myData";
+
+            Employee employee = new Employee();
+
+            employee.Name = employeeName;
+
+            Department department = new Department();
+
+            department.Name = departmentName;
+
+            department.Employees.Add(employee);
+
+            Department clone = department.DeepClone();
+        }
+    }
+}
