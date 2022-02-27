e2c18b461b59d505fdaab1c8d87e8557e928c43a
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-02-27 13:51:13 +0300

    Added project with empty documents

---
+++ BookLibrary/BookLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
