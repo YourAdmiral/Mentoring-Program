2bc19fc7c8510360fbc612e7360a3e2fad6cf4e3
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-03-20 17:08:18 +0300

    Changed listener

--- Http/Listener/Listener/Program.cs
+++ Http/Listener/Listener/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Text;
 
 namespace Listener
 {
     internal class Program
     {
-        static void Main(string[] prefixes)
+        private static void Main(string[] args)
         {
             if (!HttpListener.IsSupported)
             {
@@ -13,9 +16,7 @@ namespace Listener
                 return;
             }
             // URI prefixes are required,
-            // for example "http://contoso.com:8080/index/".
-            if (prefixes == null || prefixes.Length == 0)
-                throw new ArgumentException("prefixes");
+            var prefixes = new List<string>() { "http://localhost:8888/" };
 
             // Create a listener.
             HttpListener listener = new HttpListener();
@@ -26,20 +27,36 @@ namespace Listener
             }
             listener.Start();
             Console.WriteLine("Listening...");
-            // Note: The GetContext method blocks while waiting for a request.
-            HttpListenerContext context = listener.GetContext();
-            HttpListenerRequest request = context.Request;
-            // Obtain a response object.
-            HttpListenerResponse response = context.Response;
-            // Construct a response.
-            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-            // Get a response stream and write the response to it.
-            response.ContentLength64 = buffer.Length;
-            System.IO.Stream output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            // You must close the output stream.
-            output.Close();
+            while (true)
+            {
+                // Note: The GetContext method blocks while waiting for a request.
+                HttpListenerContext context = listener.GetContext();
+
+                HttpListenerRequest request = context.Request;
+
+                string documentContents;
+                using (Stream receiveStream = request.InputStream)
+                {
+                    using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                    {
+                        documentContents = readStream.ReadToEnd();
+                    }
+                }
+                Console.WriteLine($"Recived request for {request.Url}");
+                Console.WriteLine(documentContents);
+
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+            }
             listener.Stop();
         }
     }
