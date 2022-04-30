0039172adba224d0d473da8f78a10f60fa9effdb
Kiryl Kartashou <Kiryl_Kartashou@epam.com>
2022-04-30 16:04:35 +0300

    Initial commit

---
+++ hotel/Hotel Reservation System in c#/Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Hotel_Reservation_System.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Location { get; set; }
+        [Required]
+        public string RType { get; set; }
+        public bool Isactive { get; set; }
+        public DateTime Date { get; set; }
+
+        [NotMapped]
+        public virtual string[] RTypes => new string[] {"First Class", "2nd Class", "Third Class"};
+
+    }
+}
